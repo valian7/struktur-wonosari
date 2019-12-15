dfd6bb623e25597f69ce1dfe5e5539728060b12a
valian7 <58330049+valian7@users.noreply.github.com>
2019-12-16 05:26:40 +0700

    Add files via upload

---
+++ data/PusatPelayananLingkungan_6.js
@@ -0,0 +1,12 @@
+var json_PusatPelayananLingkungan_6 = {
+"type": "FeatureCollection",
+"name": "PusatPelayananLingkungan_6",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "id": "0", "Desa": "Teloyo" }, "geometry": { "type": "Point", "coordinates": [ 110.76018163157508, -7.625122578722741 ] } },
+{ "type": "Feature", "properties": { "id": "1", "Desa": "Gunting" }, "geometry": { "type": "Point", "coordinates": [ 110.769097763182657, -7.644995640047309 ] } },
+{ "type": "Feature", "properties": { "id": "3", "Desa": "Tegalgondo" }, "geometry": { "type": "Point", "coordinates": [ 110.705404813969039, -7.596040172963702 ] } },
+{ "type": "Feature", "properties": { "id": "4", "Desa": "Bulan" }, "geometry": { "type": "Point", "coordinates": [ 110.719390182054994, -7.629619768262575 ] } },
+{ "type": "Feature", "properties": { "id": "5", "Desa": "Bentangan" }, "geometry": { "type": "Point", "coordinates": [ 110.727225116635637, -7.618937080244792 ] } }
+]
+}
